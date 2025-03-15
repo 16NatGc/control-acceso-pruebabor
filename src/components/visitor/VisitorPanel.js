cd952734535dcf42de260ab4b02bd51c96a2dca4
16NatGc <al222310397@gmail.com>
2025-03-15 17:19:13 -0600

    Create VisitorPanel.js

---
+++ src/components/visitor/VisitorPanel.js
@@ -0,0 +1,13 @@
+// src/components/visitor/VisitorPanel.js
+import React from 'react';
+
+const VisitorPanel = () => {
+  return (
+    <div>
+      <h2>Panel de Visitante</h2>
+      <p>Este es el panel de Visitante. Aquí se agregarán las funcionalidades más adelante.</p>
+    </div>
+  );
+};
+
+export default VisitorPanel;
