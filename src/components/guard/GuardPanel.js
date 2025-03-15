20f3ef994051348e5d41892ff708f54c618b4ad7
16NatGc <al222310397@gmail.com>
2025-03-15 17:14:26 -0600

    Create GuardPanel.js

---
+++ src/components/guard/GuardPanel.js
@@ -0,0 +1,13 @@
+// src/components/guard/GuardPanel.js
+import React from 'react';
+
+const GuardPanel = () => {
+  return (
+    <div>
+      <h2>Panel de Vigilante</h2>
+      <p>Este es el panel de Vigilante. Aquí se agregarán las funcionalidades más adelante.</p>
+    </div>
+  );
+};
+
+export default GuardPanel;
